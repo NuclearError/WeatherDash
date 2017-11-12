f281bd42f3bdeda349f37935a8bfd4758c243f07
Angela Cullen <angela.cullen@immediate.co.uk>
2017-11-12 00:09:28 +0000

    Switched to csv parsing with d3 as per dc.js docs.

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 
-Papa.SCRIPT_PATH = "./papaparse.js";
-
 const url = "./data/JCMB_2015.csv";
 
-const markup = document.getElementById(output);
+$("#output").css('border', '1px solid red');
 
-const results = Papa.parse(url, {
-    worker: true,
-    download: true,
-    dynamicTyping: true,
-	complete: function(results) {
-        console.log(JSON.stringify(results.data));
-        let outputText = JSON.stringify(results.data);
-        markup.innerHTML = outputText;
-	}
+d3.csv(url, function(data) {
+    const sample = JSON.stringify(data[0]);
+    $("#output").text(sample);
 });
