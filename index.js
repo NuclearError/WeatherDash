ceeb78529d97fec16d7cda00dcac02d1366a4eb5
Angela Cullen <angela.cullen@immediate.co.uk>
2017-11-12 18:20:35 +0000

    Now showing graph for surface temp, 2nd October

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 
-const url = "./data/JCMB_2015.csv";
+// const url = "./data/JCMB_2015.csv";          // original data
+// const url = "./data/tiny.csv";               // tiny data set for dev/testing
+const url = "./data/october.csv";               // 2nd October 2015
 
 $("#output").css('border', '1px solid red');
 
+const bars = dc.barChart("#output");
+
 d3.csv(url, function(data) {
-    const sample = JSON.stringify(data[0]);
-    $("#output").text(sample);
+    const ndx = crossfilter(data);
+
+    const minDate = new Date( data[0]['date-time'] );
+    const maxDate = new Date( data[(data.length-1)]['date-time'] );
+
+    const dateDimension = ndx.dimension( (d) => {
+        return new Date(d['date-time']);
+    });
+    const temp = dateDimension.group().reduceSum( (d) => { return +d['surface temperature (C)']; });
+
+    bars
+        .width(800)
+        .height(400)
+        .x(d3.time.scale().domain([minDate,maxDate]))
+        .yAxisLabel( "Surface Temp (C)" )
+        .xAxisLabel( "Time" )
+        .dimension(dateDimension)
+        .group(temp);
+
+    bars.render();
+
 });
